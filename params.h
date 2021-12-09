f7e11690da128115d1d178a5e230410437e9503c
Tom Powell <thomastruittpowell@gmail.com>
2021-12-09 00:00:47 -0500

    moved some params to params.h
    cleanup of variable declarations

--- params.h
+++ params.h
@@ -5,6 +5,15 @@ Platform: ATmega with Arduino bootloader/firmware
 Build: Platformio
 */
 //M.K.S UNITS
+#define DEBUG 1
+#define DEBUGALT 1
+#define DEBUGIMU 1
+///////////////////////////////////////////////////////////
+//Deployment & general run params
+
+//WAIT FOR AFTER APOGEE FOR DEPLOYMENT
+const bool waitAfterAppogee = true;
+
 //PARACHUTE A DEPLOYMENT PARAMETERS
 const float deployAltitudeA = 1000;
 
@@ -20,10 +29,18 @@ const float ccstZ = -10;
 const float ccstBar = 0;
 const float ccstTem = 0;
 
-//RADIO CONFIGURATION CONSTANTS
-const char ip[15] = '255.255.255.255';
-const int freq = 915;
+//LoRaWAN CONFIGURATION CONSTANTS
+const char SelfIp[16] = "255.255.255.255";
+const char TargetIp[16] = "255.255.255.255";
+const int freq = 915;//Mhz
 
 //DATA COLLECTION PARAMETERS
-const int measurmentDelay = 65535;
+const int measurementDel = 65535;
 
+///////////////////////////////////////////////////////////
+//Startup Parameters
+
+//Number of measuements to average on startup
+const int averageCycles = 10;
+
+//
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 //written for the Arduino uno or compatible
 //switch for debug functions
-#define DEBUG 1
-#define DEBUGA 1
-#define DEBUGM 0
 #include <Arduino.h> //GNU LGPL Arduino
 #include <Wire.h> //GNU LGPL Arduino
 #include <MS5611.h> //MIT License, Rob Tillaart
@@ -12,6 +9,8 @@
 #include "../include/megapins.h" //pin table
 #include "../include/addr.h" //i2c addresses
 #include "../include/v3.hpp" //vector library
+#include "../include/altitude.h" //pressure -> altitude conversion
+#include "../params.h" //flight parameter file
 #include "ansicodes.hpp" //ANSI terminal control codes
 #define USBBAUD 115200
 //======== Functions and structs
@@ -21,10 +20,10 @@
 //======== Objects and constants
 MS5611 ms(0x77);
 Adafruit_ICM20649 icm;
-uint16_t measurement_delay_us = 65535;
-v3 lastA, lastV, pos = 0;
-float p0, t0 = 0;
-float currentAltiude = 0;
+uint16_t measurement_delay_us = measurementDel;
+float imudel_s = 0.065535;
+v3 lastacc, lastvel, pos, vel, acc = 0;
+float p0, t0, alt, p, temp = 0;
 //======== Application loop
 void setup() {
   #if DEBUG == 1
@@ -37,42 +36,48 @@ void setup() {
   icm.setGyroRange(ICM20649_GYRO_RANGE_2000_DPS); //2000 deg/sec range
 
   //Barometer Setup & initial readings
-  bool *b = ms.begin(); //start barometer
+  bool b = ms.begin(); //start barometer
   #if DEBUG == 1 
-  Serial.println(*b ? "found" : "not found");
-  delete b; // b is not used anywhere else. 
+  Serial.println(b ? "found" : "not found");
   #endif
   if (b == true){
     float *Psum, *Tsum;
-    for (int i=0; i<=10; i++){
+    for (int i=0; i<=averageCycles; i++){
       //take 10 readings from barometer/altimiter, average, and set gloabal variables
       ms.read(); //get new data
       *Psum += ms.getPressure();
       *Tsum += ms.getTemperature();
     }
     //average
-    p0 = *Psum/10;
-    t0 = *Tsum/10;
+    p0 = *Psum/averageCycles;
+    t0 = *Tsum/averageCycles;
     delete Psum, Tsum; //free memory
   }
   //MISC Setup
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); 
 }
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   //BAROMETER
   int result = ms.read();
 
-  //IMU
-  sensors_event_t accel;
-  sensors_event_t gyro;
-  sensors_event_t temp;
-  icm.getEvent(&accel, &gyro, &temp);
+  if (result == 0){
+    p=ms.getPressure();
+    temp=ms.getTemperature();
+    alt = barometric(p,p0,temp); 
+  }
 
-  lastA = v3(accel.acceleration);
-  lastV = vel;
+  //IMU
+  sensors_event_t accel, gyro, temp;
+  icm.getEvent(&accel, &gyro, &temp);
+  acc = v3(accel);
+  vel = integrate(lastacc,acc,imudel_s);
+  lastacc = acc;
+  lastvel = vel;
+
+
   #if DEBUG == 1
-  #if DEBUGA == 1
+  #if DEBUGALT == 1
   if (result != MS5611_READ_OK)
   {
     Serial.print("Error in read: ");
@@ -86,13 +91,10 @@ void loop() {
     Serial.println(ms.getPressure(), 2);
   }
   #endif
-  #if DEBUGM == 1
-  Serial.print(accel.acceleration.x);
-  Serial.print("m/s^2\t");
-  Serial.print(vel.x);
-  Serial.print("m/s\t");
-  Serial.print(pos.x);
-  Serial.print("m          \r");
+  #if DEBUGIMU == 1
+  acc.to_str("m/s2\n");
+  vel.to_str("m/s\n");
+  pos.to_str("m\n");
   #endif
   #endif
   digitalWrite(LED_BUILTIN, LOW);
