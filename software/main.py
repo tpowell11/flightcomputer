a2ee1a4c51265487d21ebf6ed854478e316c8b20
tpowell11 <thomastruittpowell@gmail.com>
2022-01-12 16:45:37 -0500

    serial read backbone of desktop done

--- software/main.py
+++ software/main.py
@@ -2,3 +2,17 @@
 # Desktop readout software for open-source flight computer
 # Python 3.9 on RasPi4
 
+# Serial IO Utilites
+import serial
+
+# Matplotlib and analysis libs
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Program Division
+try:
+    interface = serial.Serial("/dev/ttyACM0",baudrate=115200 ,timeout=1)
+    while True :
+        print(str(interface.readline()))
+except KeyboardInterrupt:
+    exit()
