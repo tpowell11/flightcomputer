19551e83b1212d3d64d935494a2c62e8b9160c26
tpowell11 <thomastruittpowell@gmail.com>
2022-01-17 16:38:17 -0500

    intial code for local rx

--- firmware/include/addr.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADDR
-#define ADDR
-const int ICM = 0x68;
-const int LIS = 0x1c;
-const int MS = 0b1110110;
-#endif
--- local/src/main.cpp
+++ local/src/main.cpp
@@ -7,14 +7,17 @@ Purpose: Ground firmware for flight controller project
 #include <Arduino.h> //GNU LGPL Arduino
 
 RH_RF95 lora(3, 2); //init of rfm95w
-
+int snr;
+uint8_t msgBuf[128];
+uint8_t msgLen;
 
 void setup() {
     bool r = lora.init(); //start rfm95w
     Serial.println(r ? "lora ok" : "lora fail");
-    
+
 }
 
 void loop() {
-
+    lora.recv(&msgBuf, &msgLen);
+    Serial.println(msgBuf);
 }
