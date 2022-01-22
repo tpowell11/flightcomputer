df5ad8f9d2026d51f661f20de6fb092f450d1caf
tpowell11 <thomastruittpowell@gmail.com>
2022-01-22 16:12:03 -0500

    uing alt. version of RH lib for PI

--- software/main.cpp
+++ software/main.cpp
@@ -4,9 +4,22 @@ Author: Tom Powell, 2022
 System: Rasbperry PI 4 (anything with g++ >=8.3.0)
 */
 #include <bcm2835.h>
+#include <wiringPi.h>
 #include <iostream>
 #include <ctime>
 #include <vector>
 #include <string>
-#include "../remote/.pio/libdeps/megaatmega2560/RadioHead/RH_RF95.h" //! FIXME bad include path
-#include "../remote/.pio/libdeps/megaatmega2560/RadioHead/RHReliableDatagram.h" //! FIXME bad include path
+#include "lib/RadioHead/RHutil/simulator.h"
+#include "lib/RadioHead/RadioHead.h"
+#include "lib/RadioHead/RHGenericDriver.h"
+#include "lib/RadioHead/RHSPIDriver.h"
+#include "lib/RadioHead/RH_RF95.h" //! FIXME bad include path
+#include "lib/RadioHead/RHReliableDatagram.h" //! FIXME bad include path
+
+RH_RF95 driver();
+ 
+int main(int argc, const char* argv[] ){
+    if(!driver.init()){
+        std::cout<<"Failed to Initialize RF95\n";
+    }
+}
